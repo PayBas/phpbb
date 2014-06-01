dd74e9adfa5210b1287ffe2af021a34d7732851f
Cesar G <prototech91@gmail.com>
2014-05-31 17:00:18 -0700

    [ticket/12013] Prevent the dropdown scrollbar from overlapping the content.
    PHPBB3-12013

--- phpBB/assets/javascript/core.js
+++ phpBB/assets/javascript/core.js
@@ -1308,6 +1308,10 @@ phpbb.toggleDropdown = function() {
 			else if ((offset + width + 2) > windowWidth) {
 				$this.css('margin-left', (windowWidth - offset - width - 2) + 'px');
 			}
+
+			// Check whether the vertical scrollbar is present.
+			$this.toggleClass('dropdown-nonscroll', this.scrollHeight === $this.innerHeight());
+
 		});
 		var freeSpace = parent.offset().left - 4;
 
